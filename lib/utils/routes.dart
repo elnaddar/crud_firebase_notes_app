417135cc17f3eae43b0555029c24dea125c3fd46
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-06-14 17:41:21 +0300

    added routes form edit/create note.

--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -1,6 +1,23 @@
+import 'package:crud_firebase_notes_app/views/note_form.dart';
 import 'package:flutter/material.dart';
 import '/pages/pages.dart';
 
 final Map<String, Widget Function(BuildContext)> routes = {
   "/": (BuildContext context) => const HomePage(),
+  "/note/create": (p0) => const NoteForm()
 };
+
+Route<dynamic>? onGenerateRoute(RouteSettings settings) {
+  if (settings.name == '/note/edit') {
+    final args = (settings.arguments as Map<String, dynamic>);
+    return MaterialPageRoute(
+      builder: (context) {
+        return NoteForm(
+          note: args['note'],
+        );
+      },
+    );
+  }
+  // Handle other routes if necessary
+  return null;
+}
