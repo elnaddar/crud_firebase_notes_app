abeef1bc8f06e607844278b3fecdc2f493c8c9a1
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-06-14 12:40:27 +0300

    created and provided instance from NotesRepository.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,21 @@
 import 'package:crud_firebase_notes_app/firebase_options.dart';
+import 'package:crud_firebase_notes_app/repositories/notes_repository.dart';
+import 'package:crud_firebase_notes_app/services/firestore_service.dart';
 import 'package:crud_firebase_notes_app/utils/routes.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const MainApp());
+  FirestoreService firestoreService = FirestoreService();
+  runApp(
+    RepositoryProvider(
+      create: (context) => NotesRepository(firestoreService: firestoreService),
+      child: const MainApp(),
+    ),
+  );
 }
 
 class MainApp extends StatelessWidget {
